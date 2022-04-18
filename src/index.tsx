f05e1d1d8be1a31a2947208694702eb49f60d178
Maxim <maxelkin5gm@gmail.com>
2022-04-18 19:14:33 +0300

    transition to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import {Provider} from "react-redux";
 import {store} from "./store/store";
@@ -8,11 +8,14 @@ import './styles/index.css';
 import 'antd/dist/antd.min.css'
 
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
+root.render(
     <React.StrictMode>
         <Provider store={store}>
             <App/>
         </Provider>
     </React.StrictMode>,
-    document.getElementById('root')
 );
